92c8256277303371e2dcebfbf405b6593bbdaead
Starcev <starcev_misha@mail.ru>
2017-10-20 22:59:20 +0500

    Наполовину сделал

--- HomeExercises/NumberValidatorTests.cs
+++ HomeExercises/NumberValidatorTests.cs
@@ -4,32 +4,92 @@ using FluentAssertions;
 using NUnit.Framework;
 
 namespace HomeExercises
-{
-	public class NumberValidatorTests
-	{
-		[Test]
-		public void Test()
-		{
-		    Action action = () => new NumberValidator(-1, 2, true);
-
+{   
+    public class NumberValidatorTests
+    {
+        [Test]
+        public void TestConstructor_ThrowArgumentException_WithNegativePrecision()
+        {
             Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        }
+        [Test]
+        public void TestConstructor_ThrowArgumentException_WithNegativeScale()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(1, -2, true));
+        }
+        [Test]
+        public void TestConstructor_ThrowArgumentException_WithScaleGreaterPrecision()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(1, 2, true));
+        }
+        [Test]
+        public void TestConstructor_DoesNotThrowException()
+        {
+            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        }
+
+        [Test]
+        public void IsValid_OnEmptyString_False()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber("").Should().BeFalse();
+        }
+
+        [Test]
+        public void IsValid_OnNull_False()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber(null).Should().BeFalse();
+        }
+
+        [Test]
+        public void IsValid_OnNonDigitString_False()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber("a.sd").Should().BeFalse();
+        }
+
+        [Test]
+        public void IsValid_OnGoodString_True()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber("0.0").Should().BeTrue();
+        }
+        [Test]
+        public void IsValid_OnGoodStringWithSign_True()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber("+0.0").Should().BeTrue();
+        }
+        [Test]
+        public void IsValid_OnGoodStringWithSpace_Flase()
+        {
+            new NumberValidator(17, 2, true).IsValidNumber(" 12.0").Should().BeFalse();
+            new NumberValidator(17, 2, true).IsValidNumber("12.0 ").Should().BeFalse();
+        }
+
+
+
+
+
+
+
+        [Test]
+        public void Test()
+        {
+            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
+            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
             Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
+            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
+            Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
+            Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
+            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
 
 //			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
 //			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
@@ -48,54 +108,54 @@ namespace HomeExercises
 //			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
 //			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
 //			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-		}
-	}
+        }
+    }
 
-	public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
+    public class NumberValidator
+    {
+        private readonly Regex numberRegex;
+        private readonly bool onlyPositive;
+        private readonly int precision;
+        private readonly int scale;
 
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
+        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+        {
+            this.precision = precision;
+            this.scale = scale;
+            this.onlyPositive = onlyPositive;
+            if (precision <= 0)
+                throw new ArgumentException("precision must be a positive number");
+            if (scale < 0 || scale >= precision)
+                throw new ArgumentException("scale must be a non-negative number less or equal than precision");
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
 
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+        public bool IsValidNumber(string value)
+        {
+            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
-			if (string.IsNullOrEmpty(value))
-				return false;
+            if (string.IsNullOrEmpty(value))
+                return false;
 
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
+            var match = numberRegex.Match(value);
+            if (!match.Success)
+                return false;
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+            // Знак и целая часть
+            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+            // Дробная часть
+            var fracPart = match.Groups[4].Value.Length;
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
+            if (intPart + fracPart > precision || fracPart > scale)
+                return false;
 
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
-		}
-	}
+            if (onlyPositive && match.Groups[1].Value == "-")
+                return false;
+            return true;
+        }
+    }
 }
